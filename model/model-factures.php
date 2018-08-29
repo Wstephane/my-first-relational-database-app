0280b0b52200c7d749e7f4f9ffa52b24894152fe
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-29 15:48:27 +0200

    Version db.php fait et début création autre DB

---
+++ model/model-factures.php
@@ -0,0 +1,14 @@
+<?php
+    include("db.php");
+
+    $id_facture = $_POST["id_facture"];
+    $date_facture = $_POST["date_facture"];
+    $numero_facture = $_POST["numero_facture"];
+    $objet_facture = $_POST["objet_facture"];
+
+
+    
+
+
+?>
+
