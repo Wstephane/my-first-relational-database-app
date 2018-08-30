9769e478d84e295a71567f7d9590eb1a76f290a2
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-30 14:41:07 +0200

    add controler

--- controller/controller.php
+++ controller/controller.php
@@ -14,4 +14,10 @@
         echo "Client";
         break;
     }
+
+    $id_facture = $_POST["id_facture"];
+    $date_facture = $_POST["date_facture"];
+    $numero_facture = $_POST["numero_facture"];
+    $objet_facture = $_POST["objet_facture"];
+
 ?>    
