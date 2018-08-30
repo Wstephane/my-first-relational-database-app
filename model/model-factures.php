49774cafb3abd470df832b05c44648377cb4b192
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-30 12:07:34 +0200

    Version mise place du forms et du model facture

--- model/model-factures.php
+++ model/model-factures.php
@@ -1,9 +1,21 @@
 <?php
     include("db.php");
 
-    $id_facture = $_POST["id_facture"];
-    $date_facture = $_POST["date_facture"];
-    $numero_facture = $_POST["numero_facture"];
-    $objet_facture = $_POST["objet_facture"];
+    if (isset($_POST['submit'])) {
+        include('../controller/controller-factures.php');
+
+        $stmt = $db->prepare("INSERT INTO facture (id_factures, numero_facture, date_facture, objet facture, societe_id_societe, personnes_id_personnes)
+        VALUES  (:id_factures, :numero_facture, :date_facture, :objet facture, : societe_id_societe, :personnes_id_personnes)");
+
+        $stmt->bindParam(':id_factures', $id_factures);
+        $stmt->bindParam(':numero_facture', $id_numero_facture);
+        $stmt->bindParam(':date_facture', $id_date_facture);
+        $stmt->bindParam(':objet facture', $id_objet_facture);
+        $stmt->bindParam(':societe_id_societe', $id_societe_id_societe);
+        $stmt->bindParam(':personnes_id_personnes', $personnes_id_personnes);
+
+        $stmt->Execute();
+        header("Location: ../vue/factures");
+    }
 ?>
 
