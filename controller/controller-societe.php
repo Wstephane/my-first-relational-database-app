02cf7f537dcb0a9cc7742ada72c7bdcea823e91e
Pedro Seromenho <pedroseromenho@pedro.local>
2018-08-30 09:05:57 +0200

    model / vue / controll societe

---
+++ controller/controller-societe.php
@@ -0,0 +1,9 @@
+<?php 
+    $id_societe = $_POST['id_societe'];
+    $nom_societe = $_POST['nom_societe'];
+    $pays_societe = $_POST['pays_societe'];
+    $tva_societe = $_POST['tva_societe'];
+    $telephone_societe = $_POST['telephone_societe'];
+    $factures_id_factures = $_POST['factures_id_factures'];
+    $personnes_id_personnes = $_POST[':personnes_id_personnes'];
+?>    
