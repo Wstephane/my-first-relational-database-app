01f243f65e58106dbaa179b9ddc007478449b402
Pedro Seromenho <pedroseromenho@pedro.local>
2018-08-30 14:21:22 +0200

    MVC ok

--- controller/controller-societe.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $id_societe = $_POST['id_societe'];
-    $nom_societe = $_POST['nom_societe'];
-    $pays_societe = $_POST['pays_societe'];
-    $tva_societe = $_POST['tva_societe'];
-    $telephone_societe = $_POST['telephone_societe'];
-    $factures_id_factures = $_POST['factures_id_factures'];
-    $personnes_id_personnes = $_POST[':personnes_id_personnes'];
-?>    
