825fbe5ef22ffcabe3c27d267e2a37f399797d54
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-30 14:26:22 +0200

    Version remaniement de add-facture

--- vue/forms/add-facture.php
+++ vue/forms/add-facture.php
@@ -5,15 +5,15 @@
 	<form action="../../model/model-factures.php" method="post">
 		<div>
 			<label for="numero">Numéro de facture</label>
-			<input type="text" name="numero_de_facture" value="">
+			<input type="number" name="numero_de_facture" value="">
 		</div>
         <div>
 			<label for="date_facture">Date de la facture</label>
-			<input type="text" name="date_facture" value="">
+			<input type="date" name="date_facture" value="">
 		</div>
 		<div>
 			<label for="objet">Objet de la facture</label>
-			<input type="number" name="objet" value="">
+			<input type="text" name="objet" value="">
 		</div>
 		<button type="submit" name="submit">Envoyer</button>
 	</form>
