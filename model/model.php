3d8f2c2672fb6a49ff22570df373270a741b5cf4
Pedro Seromenho <pedroseromenho@pedro.local>
2018-08-31 16:09:43 +0200

    validate /sanitize ok

--- model/model.php
+++ model/model.php
@@ -8,7 +8,6 @@
     $stmt = $db->prepare("INSERT INTO societe (nom_societe, pays_societe, tva_societe, telephone_societe, types_id_types) 
     VALUES (:nom_societe, :pays_societe, :tva_societe, :telephone_societe, :types_id_types)");
 
-    // $stmt->bindParam(':id_societe', $id_societe);
     $stmt->bindParam(':nom_societe', $nom_societe);
     $stmt->bindParam(':pays_societe', $pays_societe);
     $stmt->bindParam(':tva_societe', $tva_societe);
@@ -16,8 +15,16 @@
     $stmt->bindParam(':types_id_types', $types_id_types);
     
     $stmt->execute();
-    header("Location: ../public/view/frontend/societe.php");
+
+    if(count($error_list) != 0){
+      $string = '../public/view/frontend/forms/add-societe.php?status=false';
+      foreach($error_list as $index=>$value){
+          $string .= '&'.$value.'=false';
+      }
+      var_dump($string);
+      header('Location: '.$string);
+    }else{
+      header('Location: ../public/view/index.php');
+    }
   }
-
-
 ?>
---
+++ public/view/index.php
@@ -0,0 +1,7 @@
+<?php include('frontend/head.php');?>
+<body>
+<a href="frontend/societe.php">Sociétés</a>
+<a href="frontend/factures.php">Factures</a>
+<a href="frontend/clients.php">Clients</a>
+</body>
+</html>
